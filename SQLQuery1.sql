c977a84e0bb43a202be28fce0f6f3b564b8ae904
Tiago Assis <tiago.assis@aedu.com>
2017-02-20 12:39:23 -0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,21 @@
+
+create database db_QuizCinema
+
+use db_QuizCinema
+
+create table tb_Jogador(
+	id int identity primary key,
+	nome varchar(100) not null
+	sobrenome varchar (100)not null
+	email varchar (100)not null
+)
+select * from tb_Jogador
+
+create table tb_Perguntas(
+id        int identity primary key,
+pergunta           varchar(max),
+resposta_correta   varchar(max),
+id_jogador int,
+)
+
+select * from tb_Perguntas
